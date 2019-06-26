9e81832a45fa0d969a09920fb4f2614936d0ad04
iraeborn <iraeborn@gmail.com>
2019-06-26 14:09:25 -0300

    Update register.php

--- admin/register.php
+++ admin/register.php
@@ -20,4 +20,51 @@ echo $_SESSION['success'];
 unset($_SESSION['success']);
 }
 
+//Caso a sessão seja iniciada, exibe mensagem sucess
+if(isset($_SESSION['status']) && $_SESSION['status'] !='')
+{
+echo $_SESSION['status'];
+unset($_SESSION['status']);
+}
+
 ?>
+
+
+<?php
+$connection = mysqli_connect("localhost", "root","","adminpanel");
+
+$query = "SELECT * FROM register";
+$query_run = mysqli_query($connection, $query);
+
+?>
+
+<table>
+<thead>
+<tr>
+<th>Id</th>
+<th>Usuário</th>
+<th>Email</th>
+<th>Password</th>
+</tr>
+</thead>
+
+<?php
+if(mysqli_num_rows($query_run) > 0)
+{
+while($row = mysqli_fetch_assoc($query_run))
+{
+?>
+
+<tr>
+<td><?php echo $row['id']; ?></td>
+<td><?php echo $row['usuario']; ?></td>
+<td><?php echo $row['email']; ?></td>
+<td><?php echo $row['password']; ?></td>
+</tr>
+
+<?php
+}
+}
+?>
+
+</table>
