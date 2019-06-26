721fdc03b37f1a3247275668f7e5bd8463e94833
iraeborn <iraeborn@gmail.com>
2019-06-26 11:50:55 -0300

    Create login.php

---
+++ login.php
@@ -0,0 +1,10 @@
+<label class="label" for="name">Login </label>
+<select name="type" required>
+<option value="-1" disabled selected>---Selecione o tipo de usuário---</option>
+<option value="administrator">Administrador</option>
+<option value="cliente">Cliente</option>
+<option value="comercial">Comercial</option>
+<option value="marketing">Marketing</option>
+<option value="ti">Ti</option>
+<option value="juridico">Jurídico</option>
+</select>
