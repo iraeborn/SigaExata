b51c6531e2acb1558f60dd8c1b97388d29eaa758
iraeborn <iraeborn@gmail.com>
2019-06-26 13:24:28 -0300

    Update and rename cadastro.php to registro.php

--- admin/registro.php
+++ admin/registro.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <form action="code.php" method="post">
 <input type="text" name="user" placeholder="Digite seu Usuário">
 <input type="email" name="email" placeholder="Digite seu Email">
@@ -6,3 +10,14 @@
 
 <button type="submit" name="registerbtn">Salvar</button>
 </form>
+
+
+<?php
+//Caso a sessão seja iniciada, exibe mensagem sucess
+if(isset($_SESSION['success']) && $_SESSION['success'] !='')
+{
+echo $_SESSION['success'];
+unset($_SESSION['success']);
+}
+
+?>
