e771232acef2ece5939cd024e935d68b41da2965
iraeborn <iraeborn@gmail.com>
2019-06-26 17:09:15 -0300

    Update code.php

--- admin/code.php
+++ admin/code.php
@@ -1,17 +1,20 @@
 <?php
 session_start();
-$connection = mysqli_connect("localhost", "root","","adminpanel");
+$connection = mysqli_connect("localhost", "sigaexat_admin","Siga**65","sigaexat_siga");
 
 if(isset($_POST["registerbtn"])){
 
-&user = $_POST["user"];
-&email = $_POST["email"];
-&password = $_POST["password"];
-&cpassword = $_POST["confirmpassword"];
-
+$user = $_POST["user"];
+$email = $_POST["email"];
+$password = $_POST["password"];
+$cpassword = $_POST["confirmpassword"];
+$datacadastro = date("d/m/Y");
+$tipo
+$status
+	
 	if($password === $cpassword){
 
-	$query = "INSERT INTO register (username, email, password) VALUES ('$username', '$email'.'$password')";
+	$query = "INSERT INTO user (username, email, senha, datacadastro, tipo, status) VALUES ('$username', '$email'.'$password', '$datacadastro', '$tipo', 'status')";
 	$query_run = mysqli_query($connection, $query);
 		if($query_run){
 		$SESSION['sucess'] = "Usuário criado";
