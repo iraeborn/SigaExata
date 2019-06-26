0edf9bb203fe851db6d4d3cb1b65d31f08b0cb58
iraeborn <iraeborn@gmail.com>
2019-06-26 12:35:32 -0300

    Create cadastro.php

---
+++ admin/cadastro.php
@@ -0,0 +1,8 @@
+<form action="code.php" method="post">
+<input type="text" name="user" placeholder="Digite seu Usuário">
+<input type="email" name="email" placeholder="Digite seu Email">
+<input type="password" name="password" placeholder="Crie sua Senha">
+<input type="password" name="confirmpassword" placeholder="Confirme sua Senha">
+
+<button type="submit" name="registerbtn">Salvar</button>
+</form>
