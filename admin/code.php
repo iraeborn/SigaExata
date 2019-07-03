478c539157cdb69c8be3d82a71b336a724972cba
iraeborn <iraeborn@gmail.com>
2019-07-03 11:53:14 -0300

    Add files via upload

---
+++ admin/code.php
@@ -0,0 +1,52 @@
+<?php
+date_default_timezone_set('UTC');
+
+session_start();
+include 'conn.php';
+
+if(isset($_POST["registerbtn"])){
+
+function before($this, $inthat){
+        return substr($inthat, 0, strpos($inthat, $this));
+}
+$user = before('@', $_POST["email"]);
+
+$email = $_POST["email"];
+$password = $_POST["password"];
+$cpassword = $_POST["confirmpassword"];
+$datacadastro = date("d/m/Y"); 
+$tipo = 1; //nível de acesso:0 = administrador,1 = cliente,2 = comercial,3 = marketing,4 = ti, 5 = juridico
+$status = 1; //0 = ativo, 1 = desativado
+	
+	if($password === $cpassword){
+
+	$query = "INSERT INTO user (username, email, senha, datacadastro, tipo, status) VALUES ('$user','$email','$password','$datacadastro','$tipo','$status')";
+	$query_run = mysqli_query($connection, $query);
+//	mysql_close($connection);
+
+//echo $query;
+
+	if($query_run){
+	    
+		$_SESSION['sucess'] = "true";
+		$_SESSION['status'] = "Usuário criado com sucesso";
+		header('Location:login.php');
+		die();
+		
+		}else{
+		    
+		$_SESSION['status'] = "Usuário não foi criado";
+		header('Location:register.php');
+		die();
+		
+		}
+	}else{
+	    
+		$_SESSION['status'] = "O campo senha não confere";
+		header('Location:register.php');
+		die();
+		
+	} //end if is set cpassword
+
+} //end if is set post
+?>
---
+++ admin/conn.php
@@ -0,0 +1,3 @@
+<?php
+$connection = mysqli_connect("localhost", "sigaexat_admin","Siga**65","sigaexat_siga"); 
+?>
